9aaf463ed94e98fa93c08772487fd2988b670772
dlesniak <dlesniak91@gmail.com>
2020-09-19 14:06:42 +0200

    added category, photo, note models

---
+++ notes/notebook/models.py
@@ -0,0 +1,48 @@
+from django.urls import reverse
+from django.db import models
+
+
+class Category(models.Model):
+    full_name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        ordering = ['full_name', ]
+
+    def __str__(self):
+        return self.full_name
+
+    def get_absolute_url(self):
+        return reverse('university_detail', args=[str(self.id)])
+
+
+class Note(models.Model):
+    full_name = models.CharField(max_length=200)
+    description = models.CharField(max_length=2048)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='notes')
+
+    class Meta:
+        verbose_name = 'Note'
+        verbose_name_plural = 'Notes'
+        ordering = ['full_name', ]  # TODO buttons to reorder
+
+    def __str__(self):
+        return self.full_name
+
+    def get_absolute_url(self):
+        return reverse('university_detail', args=[str(self.id)])
+
+
+class Photo(models.Model):
+    full_name = models.CharField(max_length=200)
+    note = models.ForeignKey(Note, related_name='photos')
+    description = models.CharField(max_length=2048)
+
+    class Meta:
+        verbose_name = 'photo'
+        verbose_name_plural = 'photos'
+        ordering = ['note', ]
+
+    def __str__(self):
+        return self.full_name
