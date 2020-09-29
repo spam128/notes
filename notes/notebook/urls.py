dfedd47772f40869868c9f457a2a4159358406f6
dlesniak <dlesniak91@gmail.com>
2020-09-29 18:53:20 +0200

    added category views

--- notes/notebook/urls.py
+++ notes/notebook/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from notes.notebook.views import NoteCreateView, NoteListView, NoteUpdateView
+from notes.notebook.views import NoteCreateView, NoteListView, NoteUpdateView, CategoryCreateView, TypeCreateView, NoteByCategoryListView
 
 app_name = "users"
 urlpatterns = [
-    path("create/", view=NoteCreateView.as_view(), name="note-create"),
-    path("flowers/", view=NoteListView.as_view(), name="flowers-list"),
+    path("note/add/", view=NoteCreateView.as_view(), name="note-add"),
+    path("note/", view=NoteListView.as_view(), name="note-list"),
+    path("note/category/<int:pk>", view=NoteByCategoryListView.as_view(), name="note-category-list"),
     path("flowers/<slug:pk>/", view=NoteUpdateView.as_view(), name="note-detail"),
     path("food/", view=NoteListView.as_view(), name="food_recipes-list"),
+
+    path("category/add", view=CategoryCreateView.as_view(), name="category-add"),
+
+    path("type/add", view=TypeCreateView.as_view(), name="type-add"),
 
 ]
