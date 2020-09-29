ef95d20c482fa4eb9b802144e88054d37decec5c
dlesniak <dlesniak91@gmail.com>
2020-09-29 19:29:18 +0200

    added note detail url

--- notes/notebook/urls.py
+++ notes/notebook/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from notes.notebook.views import NoteCreateView, NoteListView, NoteUpdateView, CategoryCreateView, TypeCreateView, NoteByCategoryListView
+from notes.notebook.views import NoteCreateView, NoteListView, NoteUpdateView, CategoryCreateView, TypeCreateView, \
+    NoteByCategoryListView
 
 app_name = "users"
 urlpatterns = [
     path("note/add/", view=NoteCreateView.as_view(), name="note-add"),
     path("note/", view=NoteListView.as_view(), name="note-list"),
     path("note/category/<int:pk>", view=NoteByCategoryListView.as_view(), name="note-category-list"),
-    path("flowers/<slug:pk>/", view=NoteUpdateView.as_view(), name="note-detail"),
-    path("food/", view=NoteListView.as_view(), name="food_recipes-list"),
+    path("note/<int:pk>", view=NoteUpdateView.as_view(), name="note-detail"),
 
     path("category/add", view=CategoryCreateView.as_view(), name="category-add"),
 
